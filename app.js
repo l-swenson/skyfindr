eb0a1cc5ebd4c08792a5d29d0e16cec0f4eb306e
Lukas Swenson <swenson.lukas1@gmail.com>
2022-11-02 08:56:47 -0400

    Add markup to injected li.

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const clearButton = document.getElementById('clearButton');
 
 searchButton.addEventListener('click', () => {
 
-    const mailFormat = /^[A-Za-z]+$/;
+    const mailFormat = /^[A-Za-z ]+$/;
 
     if(document.getElementById('inputField').value.length === 0){
         alert("Please enter a city name!")
     }
     else if (document.getElementById('inputField').value.match(mailFormat)){
-        console.log('Add info here.')
+        weatherApi()
     }
     else {
         alert("Please enter a city name!")
@@ -22,8 +22,11 @@ searchButton.addEventListener('click', () => {
 
 });
 
-const api = 'https://api.collectapi.com/weather/getWeather?data.lang=en&data.city=Louisville'
-// const url = api + document.getElementById('inputField').value
+
+function weatherApi(){ 
+
+const input = document.getElementById('inputField').value
+const api = `https://api.collectapi.com/weather/getWeather?data.lang=en&data.city=${input}`
 
 async function fetchWeather() {
     const response = await fetch(api, {
@@ -40,7 +43,8 @@ async function fetchWeather() {
 
     const data = await response.json();
 
-    
+
+    console.log(data);
 
     const weatherToday = data.result[0]
     const menu = document.querySelector("#weatherCard")
@@ -52,12 +56,22 @@ async function fetchWeather() {
     const markup = `
 
     <h2 class="day-name">
-        ${weatherToday.day}
+        ${input}
     </h2>
     <p class="city-temp">
-        ${Math.round(weatherToday.degree)}
+        ${Math.round(weatherToday.degree)}<sup>°F</sup>
     </p>
-    
+    <figure>
+        <img class="weather-icon" src=${weatherToday.icon}>
+    </figure>
+    <p class="city-desc">
+        ${weatherToday.description}
+    </p>
+    <hr/>
+    <div id="weatherTomorrow">
+        <p>Weather Tomorrow</p>
+        ${Math.round(data.result[1].degree)}<sup>°F</sup> ${data.result[1].description}
+    </div>
 
     `;
 
@@ -66,40 +80,12 @@ async function fetchWeather() {
     menu.appendChild(li);
 
 
-    console.log(li);
-
     // displayWeather(data);
 
 }
 
 fetchWeather();
 
-// function displayWeather(data){
-//     const weatherToday = data.result[0]
-//     const weatherDiv = document.getElementById('weatherCard');
 
-//     const todaysDate = weatherToday.day;
-//     const heading = document.createElement('h2');
-//     heading.innerHTML = todaysDate;
-//     weatherDiv.appendChild(heading);
-
-//     const todaysWeather = weatherToday.degree;
-//     const para = document.createElement('p');
-//     para.innerHTML = todaysWeather;
-//     weatherDiv.appendChild(para);
-
-//     const weatherTom = data.result[1]
-//     const para1 = document.createElement('p');
-//     const tomorrowsDate = weatherTom.day;
-//     para1.innerHTML = tomorrowsDate;
-//     weatherDiv.appendChild(para1);
-
-//     const image = document.createElement('img');
-//     image.src = weatherToday.icon;
-//     weatherDiv.appendChild(image);
-    
-// };
-
-
-
+}
 
