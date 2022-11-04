0ac2f0f9f5774eb5ff5c965014491e7e8fb8ee68
Lukas Swenson <swenson.lukas1@gmail.com>
2022-11-04 16:56:34 -0400

    Added MongoDB connection.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
-const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
+
+const uri = 'mongodb+srv://lswenson:HowardLakeDB2020@cluster0.powjsj7.mongodb.net/?retryWrites=true&w=majority'
+
+function connect() {
+    try {
+        mongoose.connect{uri}
+        console.log("Connected to Mongo")
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
 const router = express.Router();
 
@@ -13,3 +25,4 @@ app.use('/api/', router);
 const server = app.listen(5000, function() {
     console.log('Server listening on port 5000!')
 })
+
