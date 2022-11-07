0790680e71050f00f57ab47413c52897af7345a1
Lukas Swenson <swenson.lukas1@gmail.com>
2022-11-07 16:43:30 -0500

    Remove Mongoose. Add connection to Mongodb and create test connection.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const {MongoClient} = require("mongodb");
 const app = express();
 
-const uri = 'mongodb+srv://lswenson:HowardLakeDB2020@cluster0.powjsj7.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://lswenson:HowardLakeDB2020@cluster0.powjsj7.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(url);
+const dbName = "test";
 
-function connect() {
+async function run() {
     try {
-        mongoose.connect{uri}
-        console.log("Connected to Mongo")
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        const col = db.collection("weather");
+
+        let weatherToday = {
+            "day": "Monday", 
+            "degree": 25.16, 
+            "description": "moderate rain"
+        }
+
+        const p = await col.insertOne(weatherToday);
+
+        const myDoc = await col.findOne();
+
+        console.log(myDoc);
+
+
+
+
+    } catch (err) {
+        console.log(err.stack);
     }
-    catch (error) {
-        console.log(error);
+    finally {
+        await client.close();
     }
 }
+
+run().catch(console.dir);
 
 const router = express.Router();
 
